5fd1eeb87e71335f28c8539a8f1f914d048a99c5
Ednei Leite da Silva <ednei@EDNEI.ednei>
2026-06-10 23:26:06 -0400

    feat(route): adicionada rota do 'Account Cards'

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\Http\Controllers\Admin\AccountCardController;
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -22,6 +24,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rota para listar e exibir a interface em Vue.js
+    Route::get('/accounts', [AccountCardController::class, 'index'])
+        ->name('accounts.index');
+
+    // Rota para submeter o formulário de criação (POST)
+    Route::post('/accounts', [AccountCardController::class, 'store'])
+        ->name('accounts.store');
+
+    // Rota para eliminar um registo específico (DELETE)
+    Route::delete('/accounts/{id}', [AccountCardController::class, 'destroy'])
+        ->name('accounts.destroy');
 });
 
 require __DIR__.'/auth.php';
