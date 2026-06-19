fc73d46637427664d4043667df56de12a6d8615a
Ednei Leite da Silva <ednei@EDNEI.ednei>
2026-06-18 22:07:38 -0400

    style: atualização de estilo

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -19,7 +19,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
         v-model="model"
         ref="input"
     />
