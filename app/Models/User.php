da061ac08a5ddbb25e0715a43174734ce2520d23
Ednei Leite da Silva <ednei@EDNEI.ednei>
2026-06-23 21:41:22 -0400

    feat(model): adicionado relaciona user com instalment_purchase

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(AccountCard::class);
     }
+
+    public function installmentPurchase(): HasMany
+    {
+        return $this->hasManu(InstallmentPurchase::class);
+    }
 }
